e922ea1639155681cb537badbff31001b7379aae
Gilles MIFSUD <gmifsud@sodifrance.fr>
2019-04-02 13:52:31 +0200

    Service refactor headers and params

--- src/app/services/mail.service.ts
+++ src/app/services/mail.service.ts
@@ -1,16 +1,16 @@
 import {Injectable} from '@angular/core';
 import {GoogleApiService} from 'ng-gapi';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {UserService} from './user.service';
-import {catchError, concatAll, flatMap, map, mergeAll, mergeMap} from 'rxjs/operators';
-import {throwError, Observable, concat, forkJoin, merge, from, of} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import {throwError} from 'rxjs';
 import {NewMail} from '../models/newMail.model';
-import { Base64 } from 'js-base64';
-
+import {Base64} from 'js-base64';
 
 @Injectable({
     providedIn: 'root'
 })
+
 export class MailService {
     private readonly API_URL: string = 'https://www.googleapis.com/gmail/v1/users';
     private readonly SEND_API_URL: string = 'https://www.googleapis.com/gmail/v1/users/me/messages/send';
@@ -21,9 +21,6 @@ export class MailService {
         private gapiService: GoogleApiService,
         private userService: UserService,
         private httpClient: HttpClient) {
-        gapiService.onLoad().subscribe(() => {
-
-        });
     }
 
     private getAuthtoken() {
@@ -34,10 +31,9 @@ export class MailService {
      * GET https://www.googleapis.com/gmail/v1/users/userId/messages
      */
     getUsersMessagesList() {
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getAuthtoken()}`);
         return this.httpClient.get(this.API_URL + '/me/messages', {
-            headers: new HttpHeaders({
-                Authorization: `Bearer ${this.getAuthtoken()}`
-            })
+            headers
         })
             .pipe(
                 map((response: any) => response.messages),
@@ -49,10 +45,9 @@ export class MailService {
      * GET https://www.googleapis.com/gmail/v1/users/userId/messages/id
      */
     getUsersMessagesDetail(id: number) {
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getAuthtoken()}`);
         return this.httpClient.get(this.API_URL + '/me/messages/' + id, {
-            headers: new HttpHeaders({
-                Authorization: `Bearer ${this.getAuthtoken()}`
-            })
+            headers
         })
             .pipe(
                 map(res => {
@@ -65,10 +60,10 @@ export class MailService {
      * GET https://www.googleapis.com/gmail/v1/users/userId/profile
      */
     getUserProfile(userId: string = 'me') {
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getAuthtoken()}`);
+
         return this.httpClient.get(this.API_URL + '/' + userId + '/profile', {
-            headers: new HttpHeaders({
-                Authorization: `Bearer ${this.getAuthtoken()}`
-            })
+            headers
         })
             .pipe(
                 map(res => {
@@ -81,13 +76,12 @@ export class MailService {
      * GET https://www.googleapis.com/gmail/v1/users/userId/threads
      */
     getThreadsList(maxResults: number = 10, labelId: string = 'INBOX') {
-        const params = new URLSearchParams();
-        params.set('labelIds', labelId);
-        params.set('maxResults', maxResults.toString());
-        return this.httpClient.get(this.API_URL + '/me/threads' + '?' + params, {
-            headers: new HttpHeaders({
-                Authorization: `Bearer ${this.getAuthtoken()}`
-            })
+        const params = new HttpParams().set('labelIds', labelId).set('maxResults', maxResults.toString());
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getAuthtoken()}`);
+
+        return this.httpClient.get(this.API_URL + '/me/threads', {
+            headers,
+            params
         })
             .pipe(
                 map((response: any) => response.threads),
@@ -99,12 +93,12 @@ export class MailService {
      * GET https://www.googleapis.com/gmail/v1/users/userId/threads/id
      */
     getThreadDetail(id: number) {
-        const params = new URLSearchParams();
-        params.set('format', 'full');
-        return this.httpClient.get(this.API_URL + '/me/threads/' + id + '?' + params, {
-            headers: new HttpHeaders({
-                Authorization: `Bearer ${this.getAuthtoken()}`
-            })
+        const params = new HttpParams().set('format', 'full');
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getAuthtoken()}`);
+
+        return this.httpClient.get(this.API_URL + '/me/threads/' + id, {
+            headers,
+            params
         })
             .pipe(
                 map((response: any) => response),
@@ -116,6 +110,10 @@ export class MailService {
      * POST https://www.googleapis.com/gmail/v1/users/userId/messages/send
      */
     sendMessage(message: NewMail) {
+        const headers = new HttpHeaders()
+            .set('Authorization', `Bearer ${this.getAuthtoken()}`)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'message/rfc822');
 
         const body = this.encodeBodySimple(
             message.to,
@@ -124,11 +122,7 @@ export class MailService {
         );
 
         return this.httpClient.post(this.SEND_ATTACHMENT_API_URL, body, {
-            headers: new HttpHeaders({
-                Authorization: `Bearer ${this.getAuthtoken()}`,
-                Accept: 'application/json',
-                'Content-Type': 'message/rfc822'
-            })
+            headers
         });
     }
 
@@ -173,10 +167,10 @@ export class MailService {
     }
 
     deleteThread(threadId: number) {
-        return this.httpClient.delete(this.API_URL + '/me/messages/' + threadId , {
-            headers: new HttpHeaders({
-                Authorization: `Bearer ${this.getAuthtoken()}`
-            })
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${this.getAuthtoken()}`);
+
+        return this.httpClient.delete(this.API_URL + '/me/messages/' + threadId, {
+            headers
         });
     }
 }
